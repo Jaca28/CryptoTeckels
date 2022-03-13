784cfede34dca0a25ee0aca859ff75f61983b996
Junior Calle <jrcalleac@gmail.com>
2022-03-12 21:05:36 -0500

    Update CryptoTeckels.sol

--- CryptoTeckels/public/solidity/CryptoTeckels.sol
+++ CryptoTeckels/public/solidity/CryptoTeckels.sol
@@ -1,75 +1,144 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+// SPDX-License-Identifier: GPL-3.0
 
-contract CertiBits{  // defino que mi contrato se va a llamar Inbox
+// Amended by HashLips
+/**
+    !Disclaimer!
+    These contracts have been used to create tutorials,
+    and was created for the purpose to teach people
+    how to create smart contracts on the blockchain.
+    please review this code on your own before using any of
+    the following code for production.
+    HashLips will not be liable in any way if for the use 
+    of the code. That being said, the code has been tested 
+    to the best of the developers' knowledge to work as intended.
+*/
 
-    uint public totalCertiBits;   // variable donde se almacena la información
-    address owner;
-    mapping (string => address[]) public hash2addressList;
-    mapping (address => mapping(string => bool)) public address2hashstate;
+//Smart contract deployed on Polygon
+//Contract address: 0x5FE831af8A790e73D93f4116a7Cc182c8a5C53E6
+//Transaction link: https://polygonscan.com/tx/0x64ee3bd1203759aa373f1185ca0bbe86cf888cd8010c36f829dd04ef50083c77
 
-    constructor () {  // este es el metodo constructor donde     inicializo el contrato con un mensaje
-        owner = msg.sender;
-    }
-    
-    uint IDu;
-    
-    function isOwner() view private returns(bool) {
-        return msg.sender == owner;    
-    }
-    
-    modifier onlyOwner {
-        require(isOwner(), "Only owner can do that!");
-        _;
-    }
-    
-    struct usuario {
-        uint IDusuario;
-        string nombre;
-        string nit;
-        string email;
-        address owner;
-    }
-    usuario[] public usuarios;
-    mapping (address => uint) public pubkey2IDu;
-    mapping (address => bool) public address2state;
-    mapping (address => uint) public saldo;
-    
-    function nuevo_usuario(string memory _nombre, string memory _nit, string memory _email, address _owner) public {
-        require(address2state[msg.sender]==false,"This Address is already subscribed to the contract");
-        usuarios.push(usuario(IDu,_nombre,_nit,_email,_owner));
-        pubkey2IDu[msg.sender]=IDu;
-        IDu+=1;
-        address2state[msg.sender]=true;
-    }
+pragma solidity >=0.7.0 <0.9.0;
 
-    function certify(string memory _hash) public { // esta función permite reemplazar el mensaje almacenada en la variable message
-        require(address2state[msg.sender],"Address not subscribed to the contract");
-        require(!address2hashstate[msg.sender][_hash],"Address already signed these document");
-        require(saldo[msg.sender]>=1,"No balance");
-        saldo[msg.sender]-=1;
-        address2hashstate[msg.sender][_hash]=true;
-        hash2addressList[_hash].push(msg.sender);
-        totalCertiBits++;
-    }
-    
-    function recargar() public payable {
-        require(address2state[msg.sender],"Address not subscribed to the contract");
-        require(msg.value>=1e18,"The value must be at least 1 Celo");
-        if( msg.value>=1e18 && msg.value<2e18 ){
-            saldo[msg.sender]+=10;
-        }
-        if(msg.value>=2e18 && msg.value<3e18){
-            saldo[msg.sender]+=25;
-        }
-        if(msg.value>=3e18){
-            saldo[msg.sender]+=50;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721Enumerable, Ownable {
+  using Strings for uint256;
+
+  string public baseURI;
+  string public baseExtension = ".json";
+  uint256 public cost = 0.01 ether;
+  uint256 public maxSupply = 300;
+  uint256 public maxMintAmount = 300;
+  bool public paused = false;
+  mapping(address => bool) public whitelisted;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
+    mint(msg.sender, maxSupply);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  // public
+  function mint(address _to, uint256 _mintAmount) public payable {
+    uint256 supply = totalSupply();
+    require(!paused);
+    require(_mintAmount > 0);
+    require(_mintAmount <= maxMintAmount);
+    require(supply + _mintAmount <= maxSupply);
+
+    if (msg.sender != owner()) {
+        if(whitelisted[msg.sender] != true) {
+          require(msg.value >= cost * _mintAmount);
         }
     }
 
-    function claimProfit(address payable _receiver) public payable onlyOwner {
-        
-        _receiver.transfer(address(this).balance);
-
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(_to, supply + i);
     }
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+
+  //only owner
+  function setCost(uint256 _newCost) public onlyOwner {
+    cost = _newCost;
+  }
+
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+    maxMintAmount = _newmaxMintAmount;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+ 
+ function whitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = true;
+  }
+ 
+  function removeWhitelistUser(address _user) public onlyOwner {
+    whitelisted[_user] = false;
+  }
+
+  function withdraw() public payable onlyOwner {
+    // This will pay HashLips 5% of the initial sale.
+    // You can remove this if you want, or keep it in to support HashLips and his channel.
+    // =============================================================================
+    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
+    require(hs);
+    // =============================================================================
+    
+    // This will payout the owner 95% of the contract balance.
+    // Do not remove this otherwise you will not be able to withdraw the funds.
+    // =============================================================================
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+    // =============================================================================
+  }
 }
